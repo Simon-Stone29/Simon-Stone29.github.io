eb741d4e9f872a56025a9c347bb8a07ae6d6e9eb
Simon Stone <simonstone029@gmail.com>
2026-02-26 23:00:36 -0500

    Implement webcam access for video stream

--- piano.js
+++ piano.js
@@ -1,2 +1,9 @@
-request camera
-assign stream to video.srcObject
+const video = document.getElementById("webcam");
+
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    video.srcObject = stream;
+  })
+  .catch(error => {
+    console.error("Camera error:", error);
+  });
